46f4095f8753b76e7ed52af87ea489d14f092a71
fooooooof <254951576@qq.com>
2019-08-14 17:17:01 +0800

    less

--- src/views/index.js
+++ src/views/index.js
@@ -7,20 +7,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Link to="/about" > ABOUT</Link>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        mathematics
       </header>
+      <section>this is a list</section>
     </div>
   );
 }
